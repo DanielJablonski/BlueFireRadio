985b7516fbdec29b872e764248c7cd4f67a224f2
Polakmaly <danieljablonski10@gmail.com>
2017-03-13 12:14:31 -0500

    Added fragment movement in the home fragment

--- app/src/main/java/com/example/bluefireradio/HomeFragment.java
+++ app/src/main/java/com/example/bluefireradio/HomeFragment.java
@@ -1,11 +1,14 @@
 package com.example.bluefireradio;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,16 +19,21 @@ import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
+    FragmentTransaction fragmentTransaction;
+
     public HomeFragment() {
         // Required empty public constructor
     }
 
+
+    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =inflater.inflate(R.layout.fragment_home, container, false);
 
@@ -39,6 +47,37 @@ public class HomeFragment extends Fragment {
         hits.setTypeface(typeface);
         albums.setTypeface(typeface);
         artists.setTypeface(typeface);
+
+        CardView topAlbum = (CardView) v.findViewById(R.id.topAlbum);
+        CardView bestMusic = (CardView) v.findViewById(R.id.bestMusic);
+        CardView topArtist = (CardView) v.findViewById(R.id.artist);
+
+        topAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, PlaylistFragment.newInstance());
+                fragmentTransaction.commit();
+            }
+        });
+
+        bestMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, MusicFragment.newInstance());
+                fragmentTransaction.commit();
+            }
+        });
+
+        topArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, ArtistFragment.newInstance());
+                fragmentTransaction.commit();
+            }
+        });
 
         return v;
     }
