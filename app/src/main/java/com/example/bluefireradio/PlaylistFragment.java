30cbeee7fc14622a6b625ceb6051a48d9c547315
Polakmaly <danieljablonski10@gmail.com>
2017-03-17 11:52:39 -0500

    Added on to playlist fragment

--- app/src/main/java/com/example/bluefireradio/PlaylistFragment.java
+++ app/src/main/java/com/example/bluefireradio/PlaylistFragment.java
@@ -1,14 +1,21 @@
 package com.example.bluefireradio;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PlaylistFragment extends Fragment {
 
+    EditText input;
 
     public PlaylistFragment() {
         // Required empty public constructor
@@ -25,9 +32,57 @@ public class PlaylistFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
+        View v= inflater.inflate(R.layout.fragment_playlist, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.addPlaylist);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Add Playlist"); //Set Alert dialog title here
+
+                // Set an EditText view to get user input
+                input = new EditText(getContext());
+                input.setHint("Enter Playlist Name");
+                alert.setView(input);
+
+                alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //You will get as string input data in this variable.
+                        // here we convert the input to a string and show in a toast.
+                        String srt = input.getEditableText().toString();
+                        Toast.makeText(getContext(),srt, Toast.LENGTH_LONG).show();
+                    } // End of onClick(DialogInterface dialog, int whichButton)
+                }); //End of alert.setPositiveButton
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                        dialog.cancel();
+                    }
+                }); //End of alert.setNegativeButton
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+            }
+        });
+        return v;
     }
 
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.playlist_creator, null))
+                // Add action buttons
+                .setPositiveButton("Add Playlist", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                });
+        return builder.create();
+    }
 }
